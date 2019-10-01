22ee50a84e22decc668d33d03785e46bd4336c4b
Kristian F Jones <me@kristianjones.xyz>
2019-10-01 10:40:34 -0500

    tests: ??

--- API/src/testSetup.ts
+++ API/src/testSetup.ts
@@ -3,6 +3,7 @@ import { createConnection } from 'typeorm';
 import { getConnectionArgs } from 'API/Library/getDbConnection';
 
 export default async function(): Promise<void> {
+  console.log('Setup Tests');
   const connection = await createConnection(getConnectionArgs(true));
   await connection.dropDatabase();
   await connection.synchronize();
