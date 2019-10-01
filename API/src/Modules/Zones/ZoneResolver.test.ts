4ca8e83e17ea523d919d4deb6a3ed304f72e3361
Kristian F Jones <me@kristianjones.xyz>
2019-10-01 11:26:12 -0500

    tests: More Zone query tests

--- API/src/Modules/Zones/ZoneResolver.test.ts
+++ API/src/Modules/Zones/ZoneResolver.test.ts
@@ -7,9 +7,124 @@ import { getTestContext } from 'API/Context';
 import { ZonePermissions, ZoneAccessPermission } from './ZonePermissionModel';
 import { Zone } from './ZoneModel';
 import { ResourceRecord } from '../ResourceRecords/ResourceRecordModel';
+import { ResourceRecordType } from '../ResourceRecords/ResourceRecordTypes';
 
 describe('Zone Model', () => {
-  test('Create Zone & Permissions', async () => {
+  test('Zones Query', async () => {
+    const user = await factory.for(User).create(1);
+
+    const zone = await factory.for(Zone).create(1);
+
+    await factory.for(ZonePermissions).create(1, {
+      zoneId: zone.id,
+      userId: user.id,
+      accessPermissions: [ZoneAccessPermission.READ],
+    });
+
+    await factory
+      .for(ResourceRecord)
+      .state('NS')
+      .create(1, { zoneId: zone.id });
+
+    const { data, errors } = await execute(
+      `query {
+      zones {
+        domainName
+        id
+        resourceRecords {
+          host
+          type
+        }
+      }
+    }`,
+      await getTestContext(user.id),
+    );
+
+    expect(data.zones[0].domainName).toBe(zone.domainName);
+    expect(data.zones[0].resourceRecords[0]).toBeDefined();
+    expect(data.zones[0].resourceRecords[0].type).toBe(ResourceRecordType.NS);
+    expect(errors).toBeUndefined();
+  });
+
+  describe('Zone Query', () => {
+    test('Authorized Access', async () => {
+      const user = await factory.for(User).create(1);
+
+      const zone = await factory.for(Zone).create(1);
+
+      await factory.for(ZonePermissions).create(1, {
+        zoneId: zone.id,
+        userId: user.id,
+        accessPermissions: [ZoneAccessPermission.READ],
+      });
+
+      const resourceRecord = await factory
+        .for(ResourceRecord)
+        .state('NS')
+        .create(1, { zoneId: zone.id });
+
+      const { data, errors } = await execute(
+        `query {
+          zone(zoneId: "${zone.id}") {
+            domainName
+            id
+            resourceRecords {
+              host
+              type
+            }
+          }
+        }`,
+        await getTestContext(user.id),
+      );
+
+      expect(data.zone.domainName).toBe(zone.domainName);
+      expect(data.zone.resourceRecords[0]).toBeDefined();
+      expect(data.zone.resourceRecords[0].type).toBe(ResourceRecordType.NS);
+      expect(data.zone.resourceRecords[0].host).toBe(resourceRecord.host);
+      expect(data.zone.id).toBe(zone.id);
+      expect(errors).toBeUndefined();
+    });
+
+    test('Unauthorized Access', async () => {
+      const authorizedUser = await factory.for(User).create(1);
+      const user = await factory.for(User).create(1);
+
+      const zone = await factory.for(Zone).create(1);
+
+      await factory.for(ZonePermissions).create(1, {
+        zoneId: zone.id,
+        userId: authorizedUser.id,
+        accessPermissions: [ZoneAccessPermission.READ],
+      });
+
+      await factory
+        .for(ResourceRecord)
+        .state('NS')
+        .create(1, { zoneId: zone.id });
+
+      const { data, errors } = await execute(
+        `query {
+            zone(zoneId: "${zone.id}") {
+              domainName
+              id
+              resourceRecords {
+                host
+                type
+              }
+            }
+          }`,
+        await getTestContext(user.id),
+      );
+
+      expect(data).toBeNull();
+      expect(errors).toBeDefined();
+      expect(errors![0].message).toBe(
+        'Access denied! You need to be authorized to perform this action!',
+      );
+    });
+  });
+
+  test('Create Zone & Permissions Mutation', async () => {
     const user = await factory
       .for(User)
       .state(UserRole.ADMIN)
@@ -35,41 +150,12 @@ describe('Zone Model', () => {
 
     expect(createZoneResponse.data.createZone.domainName).toBe('example.com');
     expect(createZoneResponse.errors).toBeUndefined();
-  });
-
-  test('User Authorized Zones', async () => {
-    const user = await factory.for(User).create(1);
-
-    const zone = await factory.for(Zone).create(1);
-
-    await factory.for(ZonePermissions).create(1, {
-      zoneId: zone.id,
-      userId: user.id,
-      accessPermissions: [ZoneAccessPermission.READ],
-    });
-
-    await factory
-      .for(ResourceRecord)
-      .state('NS')
-      .create(1, { zoneId: zone.id });
-
-    const { data, errors } = await execute(
-      `query {
-      zones {
-        domainName
-        id
-        resourceRecords {
-          host
-          type
-          value
-        }
-      }
-    }`,
-      await getTestContext(user.id),
-    );
-
-    expect(data.zones[0].domainName).toBe(zone.domainName);
-    expect(data.zones[0].resourceRecords[0]).toBeDefined();
-    expect(errors).toBeUndefined();
+    expect(
+      Zone.getUserZone(
+        user,
+        createZoneResponse.data.createZone.id,
+        ZoneAccessPermission.ADMIN,
+      ),
+    ).resolves.not.toThrow();
   });
 });
